33d9a69f4d965ca5c30da26752b185f9bf842b69
Naveen-yadav-14 <146175258+Naveen-yadav-14@users.noreply.github.com>
2024-11-06 22:11:24 +0530

    Update AboutUs.jsx

--- react-flow-app/src/AboutUs.jsx
+++ react-flow-app/src/AboutUs.jsx
@@ -4,7 +4,6 @@ function AboutUs(){
         <>
         <h1>This is aboutus file..</h1>
             <h2>about us</h2>
-            <h3>hi hello</h3>
         <p>Welcome to react classes..</p>
         </>
     )
