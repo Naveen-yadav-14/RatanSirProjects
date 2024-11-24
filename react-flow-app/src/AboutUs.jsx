19eeee7c03e85a841fcee9df8eb5b0e5b7627f90
unknown <naveendaraboina88@gmail.com>
2024-11-24 12:44:36 +0530

    this is modified

--- react-flow-app/src/AboutUs.jsx
+++ react-flow-app/src/AboutUs.jsx
@@ -6,6 +6,7 @@ function AboutUs(){
             <h2>about us</h2>
         <p>Welcome to react classes..</p>
             <p>This is changed in git hub..</p>
+            <p>this is changed in vsc code</p>
         </>
     )
 }
